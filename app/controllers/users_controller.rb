537cd7b389020dc0943e177d25dca6f6eccb2a56
asika Kuo <asika.tw@gmail.com>
2015-05-15 15:03:41 +0800

    add ransack and sort_link;

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    if params[:status]
+      if params[:status] == 'finish'
+        @cases = @user.cases_finish
+      end
+    else
+      @cases = @user.cases_ongoing
+    end
+
+    @q = @cases.ransack(params[:q])
+    @q.sorts = 'status DESC' if @q.sorts.empty?
+    @cases = @q.result.page(params[:page]).per(10)
   end
 
   protected
