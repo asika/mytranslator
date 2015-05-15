0aeba04d03dd9828f0e8e85b9190fec406b3fa6a
asika Kuo <asika.tw@gmail.com>
2015-05-15 11:51:22 +0800

    fix relationship;

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,6 +2,6 @@ class Invitation < ActiveRecord::Base
   validates_presence_of :client_id, :translator_id, :expire
 
   belongs_to :case
-  belongs_to :client, :class => "User", :foreign_key => "client_id"
-  belongs_to :translator, :class => "User", :foreign_key => "translator_id"
+  belongs_to :client, :class_name => "User", :foreign_key => "client_id"
+  belongs_to :translator, :class_name => "User", :foreign_key => "translator_id"
 end
