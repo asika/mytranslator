1ebe2b54c52346676509a568c37e110c6bd15a25
asika Kuo <asika.tw@gmail.com>
2015-06-05 10:13:04 +0800

    modify after-registeration path;

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    registered_users_path
+    if current_user.role == Role.find_by_name('client')
+      new_case_path
+    else
+      edit_user_profile_path
+    end
   end
 end
