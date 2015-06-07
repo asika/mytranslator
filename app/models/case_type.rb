4a24e56264428664dd362fe3393a8632ee141595
asika Kuo <asika.tw@gmail.com>
2015-06-07 21:57:19 +0800

    remove protected;

--- app/models/case_type.rb
+++ app/models/case_type.rb
@@ -4,8 +4,6 @@ class CaseType < ActiveRecord::Base
   belongs_to :from_language, :class_name => 'Language', :foreign_key => :from_language_id
   belongs_to :to_language, :class_name => 'Language', :foreign_key => :to_language_id
 
-  protected
-
   def name_i18n
     I18n.t("models.languages.#{self.from_language.name}")+"-->"+I18n.t("models.languages.#{self.to_language.name}")
   end
