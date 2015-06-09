797378ff9cd79e8a23c27fdb20ec21afc6c05e3e
asika Kuo <asika.tw@gmail.com>
2015-06-09 17:49:34 +0800

    add get_by_type;

--- app/models/test_source.rb
+++ app/models/test_source.rb
@@ -5,6 +5,10 @@ class TestSource < ActiveRecord::Base
   has_attached_file :source
   validates_attachment_content_type :source, :content_type => ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
 
+  def self.get_by_type(case_type)
+    self.where(:case_type => case_type).first
+  end
+
   def source_remote_url=(url_value)
     self.source = URI.parse(url_value)
     @source_remote_url = url_value
